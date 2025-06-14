063214430899a6ebdf47831ca11f980a8a8ece14
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-14 11:58:02 +0300

    Implemented is before condition (#14)
    * Implemented IsBeforeCondition.
    
    * Implemented tests for IsBeforeCondition.
    
    * Improved IsBeforeCondition.
    
    * Improved tests for IsBeforeCondition.
    
    * Code formatted.

---
+++ src/Pure.Primitives.Date.Operations/IsBeforeCondition.cs
@@ -0,0 +1,57 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.Date;
+
+namespace Pure.Primitives.Date.Operations;
+
+public sealed record IsBeforeCondition : IBool
+{
+    private readonly IEnumerable<IDate> _values;
+
+    public IsBeforeCondition(params IDate[] values) : this(values.AsReadOnly()) { }
+
+    public IsBeforeCondition(IEnumerable<IDate> values)
+    {
+        _values = values;
+    }
+
+    bool IBool.BoolValue
+    {
+        get
+        {
+            if (!_values.Any())
+            {
+                throw new ArgumentException();
+            }
+
+            IEnumerable<DateOnly> dates =
+                _values.Select(x => new DateOnly(
+                    x.Year.NumberValue,
+                    x.Month.NumberValue,
+                    x.Day.NumberValue));
+
+            DateOnly prev = DateOnly.MinValue;
+
+            foreach (DateOnly date in dates)
+            {
+                if (prev >= date)
+                {
+                    return false;
+                }
+
+                prev = date;
+            }
+
+            return true;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
